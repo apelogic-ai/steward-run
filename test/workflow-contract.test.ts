b6692a2e850441fcb776e701f2bf66409d6deabf
Leonid Belyaev <lbeliaev@gmail.com>
2026-08-08 23:47:24 -0700

    chore: prepare v0.3.3 release

--- test/workflow-contract.test.ts
+++ test/workflow-contract.test.ts
@@ -7,7 +7,7 @@ const workflowFiles = ["ci.yml", "roundtrip.yml", "release.yml", "steward-task.y
 const governedJobContainer =
   "663383948333.dkr.ecr.us-east-1.amazonaws.com/steward-run@" +
   "sha256:27235891b596debb1d8bba5f7763e14a56ce4435e2fc82f3de80122b19ff8c61";
-const actionCommit = "7e63b071d3652bc9993c545751718ae259ee0208";
+const actionCommit = "3f1e552cebe2b8b10f4634ea041f7e029cb99155";
 
 test("all external workflow actions are pinned to immutable commits", async () => {
   for (const file of workflowFiles) {
